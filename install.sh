dd339eb97f82384e268a709dd2a7ff1b13a4439d
Robbie McClellan <rlm8291@gmail.com>
2025-08-22 18:51:17 -0400

    Setup process to setup custom SDDM Theme

--- install.sh
+++ install.sh
@@ -11,6 +11,13 @@ source ~/dots/setup/config.sh
 source ~/dots/setup/development.sh
 source ~/dots/setup/desktop.sh
 
+# Install Custom SDDM Themes
+paru -S --noconfirm --needed sddm-silent-theme
+sudo cp ~/dots/sddm/sddm.conf /etc/sddm.conf
+sudo cp ~/dots/sddm/metadata.desktop /usr/share/sddm/themes/silent/
+sudo cp ~/dots/sddm/techno_city.png /usr/share/sddm/themes/silent/backgrounds/
+
+# Post Install
 sudo updatedb
 paru -Syu --noconfirm --ignore uwsm
 
