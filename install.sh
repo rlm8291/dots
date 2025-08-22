f0f4427f9433dc1dd184b946c835a4d95a0c787a
Robbie McClellan <rlm8291@gmail.com>
2025-08-22 18:25:49 -0400

    Wrap up initial install script

--- install.sh
+++ install.sh
@@ -2,9 +2,18 @@
 # Setting up my own configration installer
 cat < ~/dots/logo.txt
 
+echo "Starting Installation..."
+sleep 5
+
 # Installation Scripts
 source ~/dots/setup/aur.sh
 source ~/dots/setup/config.sh
 source ~/dots/setup/development.sh
 source ~/dots/setup/desktop.sh
 
+sudo updatedb
+paru -Syu --noconfirm --ignore uwsm
+
+echo "Restarting...
+sleep 5
+reboot
