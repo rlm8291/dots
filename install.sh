6ec70e4df2a02cb78033bd568205629ecb1ee22b
Robbie McClellan <rlm8291@gmail.com>
2025-08-24 14:51:43 +0000

    Add proper escape for bash echo

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 # Setting up my own configration installer
 cat < $HOME/dots/logo.txt
 
-echo "\nStarting Installation...\n"
+echo -e "\nStarting Installation...\n"
 sleep 5
 
 # Setup User Scripts
@@ -10,12 +10,12 @@ ln -sf "$HOME/dots/bin" "$HOME/.local/bin"
 export PATH="$HOME/.local/bin:$PATH"
 
 # Setup Symlinks for Configuration
-echo "\nSetting Up Symlinks....\n"
+echo -e "\nSetting Up Symlinks....\n"
 sleep 3
 source $HOME/dots/setup/symlinks.sh
 
 # Installation Scripts
-echo "\nInstalling Packages...\n"
+echo -e "\nInstalling Packages...\n"
 sleep 3
 
 source $HOME/dots/setup/aur.sh
@@ -25,7 +25,7 @@ source $HOME/dots/setup/desktop.sh
 source $HOME/dots/setup/mimetypes.sh
 
 # Setup Pictures Directory
-echo "\nAdding Pictures...\n"
+echo -e "\nAdding Pictures...\n"
 sleep 3
 cp -r $HOME/dots/pictures $HOME/Pictures
 
@@ -35,7 +35,7 @@ paru -Syu --noconfirm --ignore uwsm
 
 chsh -s /usr/bin/fish
 
-echo "Restarting..."
+echo -e "Restarting..."
 sleep 5
 reboot
 
