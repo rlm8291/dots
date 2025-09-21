706f45a5ae56a9180e3965bea08814c19d28b0a8
Robbie McClellan <rlm8291@gmail.com>
2025-09-21 10:11:52 -0400

    Change several configuration files + update wallpapers

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -19,4 +19,15 @@ source ~/.config/fish/alias.fish
 
 # PATH Change
 set -Ua fish_user_paths $HOME/.local/bin
+set -x GOPATH $HOME/.local/go-pkg
 
+# Global Envs
+set -x HYPRSHOT_DIR $HOME/screenshots
+
+# BEGIN opam configuration
+# This is useful if you're using opam as it adds:
+#   - the correct directories to the PATH
+#   - auto-completion for the opam binary
+# This section can be safely removed at any time if needed.
+test -r '/home/rlm8291/.opam/opam-init/init.fish' && source '/home/rlm8291/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
+# END opam configuration
--- setup/symlinks.sh
+++ setup/symlinks.sh
@@ -3,22 +3,22 @@
 DIR=$HOME/dots
 
 DOTFILES=(
-  ".config/alacritty"
-  ".config/btop"
-  ".config/fastfetch"
-  ".config/fish"
-  ".config/git"
-  ".config/hypr"
-  ".config/mako"
-  ".config/rofi"
-  ".config/systemd"
-  ".config/waybar"
-  ".config/chromium-flags.conf"
-  ".bashrc"
+  "config/alacritty"
+  "config/btop"
+  "config/fastfetch"
+  "config/fish"
+  "config/git"
+  "config/hypr"
+  "config/mako"
+  "config/rofi"
+  "config/systemd"
+  "config/waybar"
+  "config/chromium-flags.conf"
+  "bashrc"
 )
 
 for dotfile in "${DOTFILES[@]}";do
-  rm -rf "${HOME}/${dotfile}"
-  ln -sf "${DIR}/${dotfile}" "${HOME}/${dotfile}"
+  rm -rf "${HOME}/.${dotfile}"
+  ln -sf "${DIR}/${dotfile}" "${HOME}/.${dotfile}"
 done
 
